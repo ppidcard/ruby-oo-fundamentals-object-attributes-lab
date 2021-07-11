0cf9e3d78dea02f8eac0d23cc542abc1f1f27125
ppidcard <tvxqlikei@live.cn>
2021-07-12 04:58:02 +1000

    Done

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+    @name
+    @job
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+end
